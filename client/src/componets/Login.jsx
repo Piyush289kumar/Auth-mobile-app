d00179004be9932c440475c89935c0ced73db586
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-11 19:27:53 +0530

    Ui Login Build

--- client/src/componets/Login.jsx
+++ client/src/componets/Login.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
-
+import LoginPageImg from "../assets/img/mobile-med.jpg";
 function Login() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+  };
   return (
     <>
-      <div className="loginContainer bg-zinc-50 w-[50vh] h-[90vh] m-auto rounded-3xl overflow-hidden">
-        <div className="loginPage">
-          <form action="" method="post">
+      <div className="loginContainer bg-white w-[50vh] h-[90vh] m-auto rounded-3xl overflow-hidden">
+        <div className="loginPage flex flex-col justify-center">
+          <img src={LoginPageImg} alt="Login Page Img" className="px-16 py-9" />
+          <h1 className="font-bold text-2xl mx-auto font-custom mb-5">
+            Login Page
+          </h1>
+          <form
+            onSubmit={loginHandler}
+            method="post"
+            className="flex flex-col justify-center px-8"
+          >
             <input
               type="text"
               name="UserName"
@@ -15,21 +27,27 @@ function Login() {
               placeholder="User Name"
               onChange={(event) => setUserName(event.target.value)}
               value={userName}
+              className="border border-zinc-600 rounded-2xl px-4 py-2 text-zinc-800 mt-5"
             />
             <input
-              type="password"
+              type="text"
               name="password"
               id="password"
               placeholder="Password"
               onChange={(event) => setPassword(event.target.value)}
               value={password}
+              className="border border-zinc-600 rounded-2xl px-4 py-2 text-zinc-800 mt-5"
             />
-            <button type="submit">Login</button>
+            <button
+              type="submit"
+              className="bg-violet-500 text-white mt-6 w-1/2 rounded-2xl py-2 mx-auto font-bold"
+            >
+              Login
+            </button>
           </form>
         </div>
       </div>
     </>
   );
 }
-
 export default Login;
