a62129ea4c5f2c994021e4f5d3c9b774bdc7f7fe
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-11 23:12:02 +0530

    Server Live

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/get", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Live On : ${PORT}`);
+});
