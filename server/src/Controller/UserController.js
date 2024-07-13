6ef1d2e788c9f334276b776bfc0f4d59a1a15456
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-13 19:52:51 +0530

    User Register

--- server/src/Controller/UserController.js
+++ server/src/Controller/UserController.js
@@ -1,7 +1,7 @@
 import { User } from "../Models/UserModel.js";
 
 const registerUser = async (req, res) => {
-  const { userName } = req.body;
+  const { userName, password } = req.body;
   try {
     const result = await User.create({ userName, password });
     res.status(201).json(result);
